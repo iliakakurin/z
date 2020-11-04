1d33f98f981acc1cf880eb1936ad0ad5697953bf
iliakakurin <49334425+iliakakurin@users.noreply.github.com>
2020-11-04 20:19:42 +0500

    Add files via upload

---
+++ sys.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = input()
+b = input()
+corr = True
+for x in a:
+    if int(x) >= n:
+        corr = False
+for x in b:
+    if int(x) >= n:
+        corr = False
+if corr == True:
+    c = int(a, base=n)
+    d = int(b, base=n)
+    e = c + d
+    res = ''
+    while e > 0:
+        res = str(e % n) + res
+        e = e // n
+    print(res)
+else:
+    print('Wrong input!')
